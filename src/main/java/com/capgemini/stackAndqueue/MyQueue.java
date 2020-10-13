54fd60d734c9e90c083045c7822f5a3bafa5126d
abhineetpratyush <abhineetpratyush1998@gmail.com>
2020-10-14 03:15:34 +0530

    [abhinet] add : ability to create a queue of 3 integers

---
+++ src/main/java/com/capgemini/stackAndqueue/MyQueue.java
@@ -0,0 +1,20 @@
+package com.capgemini.stackAndqueue;
+
+public class MyQueue {
+	private MyLinkedList myLinkedList;
+	public MyQueue() {
+		myLinkedList = new MyLinkedList();
+	}
+
+	public INode peak() {
+		return myLinkedList.head;
+	}
+
+	public void enqueue(INode myNode) {
+		myLinkedList.append(myNode);
+	}
+
+	public void printQueue() {
+		myLinkedList.printMyNodes();
+	}
+}
